afa28774aa0a438383aa8bb07c5968a742e8ae9d
Ssempewo <83715849+Ssempewo@users.noreply.github.com>
2022-09-22 12:49:11 +0300

    Add files via upload

---
+++ P4E Exercise Chapter 2.py
@@ -0,0 +1,29 @@
+# Exercise 2
+name = input("Enter your name: ")
+print("Hello " + name)
+
+# Exercise 3
+hours = int(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+print(f"Pay: {hours * rate}")
+
+# Exercise 4
+width = 17
+height = 12.0
+
+#  width//2 integer division, returns an integer (whole number)
+8
+
+#  width/2.0 float division, returns a float (Decimal number)
+8.5
+
+# height/3 float division, returns a float (Decimal number)
+4.0
+
+# 1+2*5 consider precedences in the order of P.O.D.M.A.S
+11
+
+# Exercise 5
+celsius = float(input("Enter degrees celsius: "))
+fahrenheit = (9/5 * celsius) + 32
+print(f"Fahrenheit: {fahrenheit}")
